86bb8c8667a3f2cb4ebcddf4992a60841424e989
Raulkg <92127255+kishoreapps@users.noreply.github.com>
2022-04-27 21:30:13 -0700

    add amplify auth

--- src/index.js
+++ src/index.js
@@ -4,6 +4,12 @@ import './index.css';
 import App from './App';
 import reportWebVitals from './reportWebVitals';
 
+import { withAuthenticator } from '@aws-amplify/ui-react';
+import '@aws-amplify/ui-react/styles.css';
+
+//import awsExports from './aws-exports';
+// Amplify.configure(awsExports);
+
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <React.StrictMode>
